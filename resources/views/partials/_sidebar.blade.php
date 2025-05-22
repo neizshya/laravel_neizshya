863dbf921dd560b4c85745a72820a4030aae98f0
neizshya <nurul.adzan24@gmail.com>
2025-05-22 20:50:22 +0700

    change title on sidebar

--- resources/views/partials/_sidebar.blade.php
+++ resources/views/partials/_sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 100%;"> <a href="/"
         class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"> <span
-            class="fs-4">Teramedika</span> </a>
+            class="fs-4">Teramedik</span> </a>
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
         <li class="nav-item"> <a href="{{ route('home') }}"
